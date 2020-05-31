45e54d99b8426d639032c2e059de70f60df68105
luhcasgabriel <lucastec_adm82@hotmail.com>
2020-05-31 05:12:50 -0300

    alterações
    Signed-off-by: luhcasgabriel <lucastec_adm82@hotmail.com>

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -16,7 +16,16 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, // usuario que criou esse spot - vai gravar o id na base e é possível depois pegar esse id para outras operações
         ref: 'User' // ref - referencia de qual model está se referindo essa informação, no caso o usuário que criou o spot
     }  
+}, {
+    toJSON: {
+        virtuals: true, /* parametro a mais no schema, para permitir que virtuals sejam carregados junto ao json de resposta */
+    },
 });
+
+/* virtual - será carregado pelo schema acima */
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`
+})
 
 //  export = criação do model Spot, só a partir daqui é criado realmente o model
 //  passado o nome da model (Spot) e o schema que ele usa (estrutura do Spot)
--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const routes = require('./routes');
 /* import mongoose - gerencia banco de dados mongoDB na núvem*/
 const mongoose = require('mongoose');
 
+const path = require('path');
+
+const cors = require('cors');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ynfcm.mongodb.net/omnistack09?retryWrites=true&w=majority', {
@@ -16,7 +20,15 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ynfcm.mongodb.net/
 /* por padrão o express não entende requisições no formato json 
    é necessário informa-lo
 */
+
+/*permite que qualquer aplicação acesse o backend - a não ser que tenha parametros indicando { origin: 'http://local:333'} */
+app.use(cors());
+
+
 app.use(express.json());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads'))); /* quando o usuario usar a rota /files - vai setar staticamente um camimho pra ele - direcionando para a pasta de imagens - __dirname (pega o caminho do arquivo atual - (..) retorna uma pasta e entra em uploads) */
+
 // usando o arquivo importado
 //  lembrando que sempre deve vir após o use express.json
 app.use(routes);
